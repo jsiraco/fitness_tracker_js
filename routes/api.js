3d0ac71c69bbf1e8f445046fbd25fab0dc84c454
Jas <ragnarock.josef@gmail.com>
2021-11-08 16:17:45 -0500

    added rworkout range

--- routes/api.js
+++ routes/api.js
@@ -11,24 +11,44 @@ router.get("/workouts", (req, res) => {
         });
 });
 
-router.get("/workouts/range", (req, res) => {
-    Workout.find({})
-        .sort({ day: -1 })
-        .then(dbWorkout => {
-            res.json(dbWorkout);
-        }).catch(err => {
-            res.status(400).json(err);
-        });
+router.post("/workouts", async ({ body }, res) => {
+    try {
+        const workout = req.body;
+        const addWorkout = await Workout.create({ exercises: workout });
+        res.status(200).json(addWorkout);
+    } catch (err) {
+        res.status(400).json(err);
+    }
 });
 
-router.post("/workouts", ({ body }, res) => {
-    Workout.create(body)
-        .then(body => {
-            res.json(body);
-        }).catch(err => {
-            res.status(400).json(err);
-        });
+router.get("/workouts/range", async (req, res) => {
+    try {
+        const data = await Workout.find().limit(7);
+        if (data) {
+            Workout.aggregate(
+                [
+                    {
+                        $addFields: {
+                            totalDuration: {
+                                $sum: "$exercises.duration",
+                            },
+                        },
+                    },
+                ],
+                (err, data) => {
+                    if (err) {
+                        res.status(400).json(err);
+                    } else {
+                        res.json(data);
+                    }
+                }
+            );
+        }
+    } catch (err) {
+        res.status(500).json(err);
+    }
 });
+
 
 router.put("/exercise", ({ body }, res) => {
     router.put("/:id", async (req, res) => {
