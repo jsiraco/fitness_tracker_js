d9f4e1a324bd4b097da85afb0e8f1d8cb4d0a049
Jas <ragnarock.josef@gmail.com>
2021-11-08 16:04:13 -0500

    added workout range route

--- routes/api.js
+++ routes/api.js
@@ -1,53 +1,49 @@
 const router = require("express").Router();
-const path = require("path");
 const Workout = require("../models/workout.js");
 
-router.get("/api/workouts", (req, res) => {
+router.get("/workouts", (req, res) => {
     Workout.find({})
-    .sort({ day: -1 })
-    .then(dbWorkout => {
-        res.json(dbWorkout);
-    }).catch(err => {
-        res.status(400).json(err);
-    });
+        .sort({ day: -1 })
+        .then(dbWorkout => {
+            res.json(dbWorkout);
+        }).catch(err => {
+            res.status(400).json(err);
+        });
 });
 
-router.get("/api/workouts/range", (req, res) => {
+router.get("/workouts/range", (req, res) => {
     Workout.find({})
-    .then(dbWorkout => {
-        res.json(dbWorkout);
-    }).catch(err => {
-        res.status(400).json(err);
-    });
+        .sort({ day: -1 })
+        .then(dbWorkout => {
+            res.json(dbWorkout);
+        }).catch(err => {
+            res.status(400).json(err);
+        });
 });
 
-router.get("/stats", (req, res) => {
-    Workout.find({})
-    .sort({ day: -1 })
-    .then(dbWorkout => {
-        res.json(dbWorkout);
-    }).catch(err => {
-        res.status(400).json(err);
-    });
-});
-
-router.get("/exercise", (req, res) => {
-    Workout.find({})
-    .sort({ day: -1 })
-    .then(dbWorkout => {
-        res.json(dbWorkout);
-    }).catch(err => {
-        res.status(400).json(err);
-    });
-});
-
-router.post("api/exercise", ({body}, res) => {
+router.post("/workouts", ({ body }, res) => {
     Workout.create(body)
-    .then(dbWorkout => {
-        res.json(dbWorkout);
-    }).catch(err => {
-        res.status(400).json(err);
+        .then(body => {
+            res.json(body);
+        }).catch(err => {
+            res.status(400).json(err);
+        });
+});
+
+router.put("/exercise", ({ body }, res) => {
+    router.put("/:id", async (req, res) => {
+        try {
+            const query = { _id: mongojs.ObjectId(req.params.id) };
+            const workout = req.body;
+            const addExercise = await Workout.updateOne(query, {
+                $push: { exercises: workout },
+            });
+            res.status(200).json(addExercise);
+        } catch (err) {
+            res.status(500).json(err);
+        }
     });
 });
+
 
 module.exports = router;
