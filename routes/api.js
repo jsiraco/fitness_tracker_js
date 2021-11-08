617af5f7fa8944c58ed0053fb44ad953aa71f249
Jas <ragnarock.josef@gmail.com>
2021-11-08 15:25:47 -0500

    removed hoemroutes

--- routes/api.js
+++ routes/api.js
@@ -1,7 +1,6 @@
 const router = require("express").Router();
+const path = require("path");
 const Workout = require("../models/workout.js");
-
-// router.post("/api/")
 
 router.get("/api/workouts", (req, res) => {
     Workout.find({})
@@ -13,7 +12,16 @@ router.get("/api/workouts", (req, res) => {
     });
 });
 
-router.get("/api/stats", (req, res) => {
+router.get("/api/workouts/range", (req, res) => {
+    Workout.find({})
+    .then(dbWorkout => {
+        res.json(dbWorkout);
+    }).catch(err => {
+        res.status(400).json(err);
+    });
+});
+
+router.get("/stats", (req, res) => {
     Workout.find({})
     .sort({ day: -1 })
     .then(dbWorkout => {
@@ -23,9 +31,18 @@ router.get("/api/stats", (req, res) => {
     });
 });
 
-router.get("/api/exercise", (req, res) => {
+router.get("/exercise", (req, res) => {
     Workout.find({})
     .sort({ day: -1 })
+    .then(dbWorkout => {
+        res.json(dbWorkout);
+    }).catch(err => {
+        res.status(400).json(err);
+    });
+});
+
+router.post("api/exercise", ({body}, res) => {
+    Workout.create(body)
     .then(dbWorkout => {
         res.json(dbWorkout);
     }).catch(err => {
