94c610abfb7b842638e6b2207ecd0c2b106c95b2
Jas <ragnarock.josef@gmail.com>
2021-11-08 16:53:29 -0500

    added workout duration

--- routes/api.js
+++ routes/api.js
@@ -1,30 +1,40 @@
 const router = require("express").Router();
 const Workout = require("../models/workout.js");
 
-router.get("/workouts", (req, res) => {
-    Workout.find({})
-        .sort({ day: -1 })
-        .then(dbWorkout => {
-            res.json(dbWorkout);
-        }).catch(err => {
-            res.status(400).json(err);
-        });
-});
-
-router.post("/workouts", async ({ body }, res) => {
+router.get("/workouts", async (req, res) => {
     try {
-        const workout = req.body;
-        const addWorkout = await Workout.create({ exercises: workout });
-        res.status(200).json(addWorkout);
+    const dbWorkout = await Workout.find();
+    if (dbWorkout) {
+        Workout.aggregate(
+            [
+                {
+                    $addFields: {
+                        totalDuration: {
+                            $sum: "$exercises.duration",
+                        },
+                    },
+                },
+            ],
+            (err, data) => {
+                if (err) {
+                    res.status(400).json(err);
+                } else {
+                    res.json(data);
+                }
+            }
+        )
+    }
     } catch (err) {
         res.status(400).json(err);
+
     }
 });
 
+// Range of last 7 workouts
 router.get("/workouts/range", async (req, res) => {
     try {
-        const data = await Workout.find().limit(7);
-        if (data) {
+        const dbWorkout = await Workout.find().limit(7);
+        if (dbWorkout) {
             Workout.aggregate(
                 [
                     {
@@ -49,20 +59,27 @@ router.get("/workouts/range", async (req, res) => {
     }
 });
 
+router.post("/workouts", async ({ body }, res) => {
+    try {
+        const workout = req.body;
+        const addWorkout = await Workout.create({ exercises: workout });
+        res.status(200).json(addWorkout);
+    } catch (err) {
+        res.status(400).json(err);
+    }
+});
 
-router.put("/exercise", ({ body }, res) => {
-    router.put("/:id", async (req, res) => {
-        try {
-            const query = { _id: mongojs.ObjectId(req.params.id) };
-            const workout = req.body;
-            const addExercise = await Workout.updateOne(query, {
-                $push: { exercises: workout },
-            });
-            res.status(200).json(addExercise);
-        } catch (err) {
-            res.status(500).json(err);
-        }
-    });
+router.put("/workouts/:id", async (req, res) => {
+    try {
+        const query = { _id: mongojs.ObjectId(req.params.id) };
+        const workout = req.body;
+        const addExercise = await Workout.updateOne(query, {
+            $push: { exercises: workout },
+        });
+        res.status(200).json(addExercise);
+    } catch (err) {
+        res.status(500).json(err);
+    }
 });
 
 
